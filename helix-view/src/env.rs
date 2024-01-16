f05ee96cfb4bb3a24628535fac20c91deb15780a
zerosign <r1nlx0@gmail.com>
2024-01-16 13:06:46 +0700

    feat: add `env_var_is_eq` for checks value of env
    add helper function `env_var_is_eq` in `crate::env` for checking
    value of the envs.

--- helix-view/src/env.rs
+++ helix-view/src/env.rs
@@ -6,3 +6,10 @@ pub fn binary_exists(binary_name: &str) -> bool {
 pub fn env_var_is_set(env_var_name: &str) -> bool {
     std::env::var_os(env_var_name).is_some()
 }
+
+#[cfg(not(windows))]
+pub fn env_var_is_eq(env_var_name: &str, value: &str) -> bool {
+    std::env::var_os(env_var_name)
+        .iter()
+        .all(|v| v.eq_ignore_ascii_case(value))
+}
